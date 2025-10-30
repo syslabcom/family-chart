b62e8d00b8ce83ef52037afb5d8759b135f13079
Alexander Pilz <pilz@syslab.com>
2025-10-30 09:44:35 +0100

    Fix for missing parents in main element.

--- src/layout/handlers.ts
+++ src/layout/handlers.ts
@@ -99,7 +99,13 @@ export function setupSiblings({
   const main_mother_id = main.data.rels.mother
 
   const siblings = findSiblings(main)
-  if (siblings.length > 0 && !main.parents) throw new Error('no parents')
+  if (siblings.length > 0 && !main.parents) {
+    if (main.data.parents) {
+      main.parents = main.data.parents;
+    } else {
+      throw new Error('no parents');
+    }
+  }
   const siblings_added = addSiblingsToTree(main)
   positionSiblings(main)
 
